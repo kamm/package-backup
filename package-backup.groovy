b3429c1ec5a8b6d99077c2a8e0246c90456e9c1f
Kamil Metrak <kamil.metrak@coi.gov.pl>
2016-02-26 11:21:34 +0100

    added handler for different types of objects

--- package-backup.groovy
+++ package-backup.groovy
@@ -1,43 +1,34 @@
 import groovy.sql.Sql
 
 def classLoader = this.getClass().getClassLoader();
-while (!classLoader.getClass().getName().equals("org.codehaus.groovy.tools.RootLoader")) {
+while (!classLoader.getClass().getName().equals("org.codehaus.groovy.tools.RootLoader")) 
+{
   classLoader = classLoader.getParent()
 }
 
 groovy.grape.Grape.grab(group:'com.oracle', module:'ojdbc5', version:'[11.2.0.3.0,)', classLoader: classLoader)
 
-class SqlTool {
+class SqlTool
+{
   private sql;
  
-  public SqlTool(url, driver, username, password) {
+  public SqlTool(url, driver, username, password) 
+  {
     this.sql = Sql.newInstance(url, username, password, driver)
     this.sql.eachRow('''
-			select 
-				object_name, 
-				object_type, 
-				decode(
-					t.object_type, 
-					'PACKAGE', 'pks', 
-					'PACKAGE BODY', 'pkb',
-					object_name) ext, 
-				dbms_metadata.get_ddl(
-					decode(
-						t.object_type, 
-						'PACKAGE', 'PACKAGE_SPEC', 
-						'PACKAGE BODY', 'PACKAGE_BODY'
-					),object_name) ddl 
-			from 
-				user_objects t 
-			where 
-				t.object_type in ('PACKAGE', 'PACKAGE BODY')
-				'''){
-		row ->
+        SELECT OBJECT_NAME,
+			   OBJECT_TYPE,
+			   DECODE(T.OBJECT_TYPE, 'PACKAGE', 'pks', 'PACKAGE BODY', 'pkb', 'TYPE', 'type', 'FUNCTION', 'fun', 'TRIGGER', 'trg', 'PROCEDURE', 'proc', 'TABLE', 'tab', 'INDEX', 'idx', 'VIEW', 'view', 'SYNONYM', 'syn', 'SEQUENCE', 'seq', OBJECT_TYPE) EXT,
+			   DBMS_METADATA.GET_DDL(DECODE(T.OBJECT_TYPE, 'PACKAGE', 'PACKAGE_SPEC', 'PACKAGE BODY', 'PACKAGE_BODY', T.OBJECT_TYPE), OBJECT_NAME) DDL
+		  FROM USER_OBJECTS T
+		 WHERE T.OBJECT_TYPE <> 'LOB'
+	''')
+	{row ->
 		def filename = "${row.object_name}.${row.ext}"
 		File file = new File(filename)
 		file << row.ddl.getCharacterStream().getText()
 		println filename
-	}
+    }
   }
 }
  
